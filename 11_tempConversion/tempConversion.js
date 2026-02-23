498f0bd09fc2c612ccfc521fda3ac91dcc61b6ab
JeanPCB <92955454+JeanPCB@users.noreply.github.com>
2026-02-23 12:35:04 -0300

    Finish exercise 11 - number, decimals, precedence

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degrees) {
+  // x °F ≘ (x − 32) × ⁠5/9⁠ °C
+  return Number((((degrees - 32) * 5) / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degrees) {
+  // x °C ≘ (x × ⁠9/5⁠ + 32) °F
+  return Number(((degrees * 9) / 5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
