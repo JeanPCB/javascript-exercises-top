2f9778df795b98aaf540a30bf7d6019b004d6638
JeanPCB <92955454+JeanPCB@users.noreply.github.com>
2026-02-23 12:05:40 -0300

    Finish exercise 10 - logical/arithmetical operators

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const isYearDivByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isYearDivByFourHund = year % 400 === 0;
 
+  if (isYearDivByFour && (!isCentury || isYearDivByFourHund)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
